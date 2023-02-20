6549008111646efedda7856893c6fab829cb033f
Karl Nicoll <karl@karlnicoll.net>
2023-02-20 00:27:14 +0000

    Remove CTRL/Minus Space Cadet Key
    This setting causes me grief (e.g. inability to do CTRL+-), so the
    dedicated underscore key has now been removed in favour of a more
    "classic" "-/_" key.
    
    I intend to redo CTRL as a space cadet key again in the future, but for
    now I need to untrain some muscle memory.

--- keymap.c
+++ keymap.c
@@ -19,11 +19,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY (default)
  * ,-------------------------------------------.                    ,-------------------------------------------.
- * |   `    |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  _     |
+ * |   `    |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -     |
  * |--------+------+------+------+------+------|                    |------+------+------+------+------+--------|
  * |  Tab   |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  =     |
  * |--------+------+------+------+------+------|                    |------+------+------+------+------+--------|
- * |LCTRL/- |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '     |
+ * |LCTRL   |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '     |
  * |--------+------+------+------+------+------|   {   |    |   }   |------+------+------+------+------+--------|
  * |LSHIFT/(|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RSHIFT/)|
  * `-------------------------------------------/       /    \       \-------------------------------------------'
@@ -32,9 +32,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                    `-----------------------------'          '-------''-------------------'
  */
   [_QWERTY] = LAYOUT(
-    KC_GRV,          KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_UNDS,
+    KC_GRV,          KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
     KC_TAB,          KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,
-    LCTL_T(KC_MINS), KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LCTL,         KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSPO,         KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, UK_LCBR,     UK_RCBR,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
                                 KC_LGUI, LALT_T(KC_LBRC), TT(1), KC_SPC,  KC_ENT, TT(2), RALT_T(KC_RBRC), KC_BSPC
   ),
